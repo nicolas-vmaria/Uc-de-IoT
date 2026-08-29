3c9ce60ea07dd8ce3a4b5349f8b55282bf200af4
Nicolas Vieira Maria <nicolasvmaria1@gmail.com>
2026-08-29 09:36:41 -0300

    finalizando desafio

--- aula 04/while/desafioHardcore.c
+++ aula 04/while/desafioHardcore.c
@@ -6,14 +6,14 @@ int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
-    int cont = 0;
-    float precoUnit, custoEstocagem, imposto, precoFinal;
+    int cont = 0,contBarato = 0, contNormal = 0, contCaro = 0;
+    float precoUnit, custoEstocagem, imposto, precoFinal, mediaAdicional = 0, maiorFinal = 0, menorFinal = 0,totalImposto = 0;
     char refrigeracao, categoria, nome[100];
 
-    while(cont < 3){
+    while(cont < 12){
 
         printf("\nDigite o nome do produto: ");
-        scanf(" %99s", nome);
+        scanf(" %99[^\n]", nome);
         printf("Digite o preço do produto: ");
         scanf("%f", &precoUnit);
         printf("Digite S para refrigerado ou N para não refrigerado: ");
@@ -27,12 +27,15 @@ int main(){
 
             if(categoria == 'A'){
                 custoEstocagem = 2;
+                mediaAdicional += custoEstocagem;
             }
             else if(categoria == 'L'){
                 custoEstocagem = 3;
+                mediaAdicional += custoEstocagem;
             }
             else if(categoria == 'V'){
                 custoEstocagem = 4;
+                mediaAdicional += custoEstocagem;
             }
 
         }
@@ -40,9 +43,11 @@ int main(){
 
             if(refrigeracao == 'S'){
                 custoEstocagem = 6;
+                mediaAdicional += custoEstocagem;
             }
             else{
                 custoEstocagem = 0;
+                mediaAdicional += custoEstocagem;
             }
         }
         else{
@@ -51,21 +56,26 @@ int main(){
 
                 if(categoria == 'A'){
                     custoEstocagem = 5;
+                    mediaAdicional += custoEstocagem;
                 }
                 else if(categoria == 'L'){
                     custoEstocagem = 2;
+                    mediaAdicional += custoEstocagem;
                 }
                 else if(categoria == 'V'){
                     custoEstocagem = 4;
+                    mediaAdicional += custoEstocagem;
                 }
             }
             else{
 
                 if(categoria == 'A' || categoria == 'V'){
                     custoEstocagem = 0;
+                    mediaAdicional += custoEstocagem;
                 }
                 else if(categoria == 'L'){
                     custoEstocagem = 1;
+                    mediaAdicional += custoEstocagem;
                 }
             }
         }
@@ -74,9 +84,13 @@ int main(){
 
         if(categoria == 'A' && refrigeracao == 'S'){
             imposto = precoUnit * 0.04;
+            mediaAdicional += imposto;
+            totalImposto += imposto;
         }
         else{
             imposto = precoUnit * 0.02;
+            mediaAdicional += imposto;
+            totalImposto += imposto;
         }
 
         // Preço final
@@ -87,8 +101,44 @@ int main(){
         printf("\nImposto: R$ %.2f", imposto);
         printf("\nPreço final: R$ %.2f\n", precoFinal);
 
+        //Classificaçâo
+
+        if(precoFinal < 20){
+            printf("\nProduto barato");
+            contBarato++;
+        }
+        else if(precoFinal >= 20 && precoFinal <= 100){
+            printf("\nProduto normal");
+            contNormal++;
+        }
+        else{
+            printf("\nProduto caro");
+            contCaro++;
+        }
+
+        if(cont == 0){
+            maiorFinal = precoFinal;
+            menorFinal = precoFinal;
+        }
+        else{
+            if(precoFinal > maiorFinal){
+                maiorFinal = precoFinal;
+            }
+            if(precoFinal < menorFinal){
+                menorFinal = precoFinal;
+            }
+        }
+
         cont++;
     }
+    mediaAdicional /= cont;
+    printf("\nMédia dos custos adicionais: R$ %.2f", mediaAdicional);
+    printf("\nMaior preço final: R$ %.2f", maiorFinal);
+    printf("\nMenor preço final: R$ %.2f", menorFinal);
+    printf("\nTotal de impostos: R$ %.2f", totalImposto);
+    printf("\nQuantidade de produtos baratos: %d", contBarato);
+    printf("\nQuantidade de produtos normais: %d", contNormal);
+    printf("\nQuantidade de produtos caros: %d", contCaro);
     system("pause");
     getchar();   
 }
